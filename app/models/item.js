834ff6b47da1f837d383c1592b091808aa0b227f
Randall Morey <randall@randallmorey.com>
2019-12-01 19:05:52 -0500

    feat: 🎸 automatically add dateCreated and dateUpdated to models

--- app/models/item.js
+++ app/models/item.js
@@ -2,6 +2,8 @@ import Model from '@ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 import Validator from '../mixins/object-validator';
+import DatedModelAttributesMixin from '../mixins/dated-model-attributes';
+import DatedModelMethodsMixin from '../mixins/dated-model-attributes';
 
 /**
  * A simple model representing the universe of available shopping items.
@@ -9,11 +11,17 @@ import Validator from '../mixins/object-validator';
  *
  * @class ItemModel
  * @augments Model
+ * @augments Validator
+ * @augments DatedModelAttributesMixin
+ * @augments DatedModelMethodsMixin
  *
  * @property name {String}
  * @property category {ItemCategoryModel}
  */
-export default class ItemModel extends Model.extend(Validator) {
+export default class ItemModel extends Model.extend(
+  Validator,
+  DatedModelAttributesMixin,
+  DatedModelMethodsMixin) {
 
   // =attributes
 

43fd2b39d138799f8d593a0fc5fceba5ca955269
fernandogelin <fernando@fernandogelin.com>
2019-12-21 11:40:16 -0500

    feat: add list and item related components

--- tests/integration/components/list/empty-list-message-test.js
+++ tests/integration/components/list/empty-list-message-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | list/empty-set-message', function(hooks) {
+module('Integration | Component | list/empty-list-message', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<List::EmptySetMessage />`);
+    await render(hbs`<List::EmptyListMessage />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <List::EmptySetMessage>
+      <List::EmptyListMessage>
         template block text
-      </List::EmptySetMessage>
+      </List::EmptyListMessage>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');

5ffc10fac6a71e5d1275bc0aef1ef585b2555a14
Randall Morey <randall@randallmorey.com>
2019-12-21 12:44:58 -0500

    chore(list): add comments

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -14,10 +14,12 @@ export default class ItemsController extends Controller {
   // =properties
 
   /**
-   * 
+   * Array of category items groups.
+   * @type {{category: Object, items: Array}[]}
    */
   @computed('model.@each.category')
   get groupedItems() {
+    // Group items into category objects, with fields category and items.
     const groupObject = this.model.reduce((accumulator, currentValue) => {
       const categoryId = currentValue.get('category.id');
       if (accumulator[categoryId]) {
@@ -30,6 +32,8 @@ export default class ItemsController extends Controller {
       }
       return accumulator;
     }, {});
+    // Transform object into an array of category objects, since these
+    // are easier to work with.
     const groupArray = Object.keys(groupObject).map(key => groupObject[key]);
     return groupArray;
   }

0fc89b8ec813eff076df21734b06e618a3b7ac57
Randall Morey <randall@randallmorey.com>
2019-12-21 12:24:38 -0500

    chore(list): group items by categories

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 /**
  * Exposes the active pane service to the context.
@@ -10,4 +11,26 @@ export default class ItemsController extends Controller {
 
   @service activePane;
 
+  // =properties
+
+  /**
+   * 
+   */
+  @computed('model.@each.category')
+  get groupedItems() {
+    const groupObject = this.model.reduce((accumulator, currentValue) => {
+      const categoryId = currentValue.get('category.id');
+      if (accumulator[categoryId]) {
+        accumulator[categoryId].items.push(currentValue);
+      } else {
+        accumulator[categoryId] = {
+          category: currentValue.get('category'),
+          items: [currentValue]
+        };
+      }
+      return accumulator;
+    }, {});
+    const groupArray = Object.keys(groupObject).map(key => groupObject[key]);
+    return groupArray;
+  }
 }

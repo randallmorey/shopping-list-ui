46ff38e16916690a73ee11cfd5f35d3bd55501d3
Randall Morey <randall@randallmorey.com>
2019-12-22 14:14:50 -0500

    refactor: 💡 clean up mirage scenarios

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,27 +1,10 @@
+import development from './development';
+import empty from './empty';
 import ENV from '../../config/environment';
-import emptyList from './empty-list';
-
-const scenarios = {
-  emptyList
-}
-
-const activeScenario = ENV.MIRAGE_SCENARIO;
 
 export default function(server) {
-
+  const scenarios = {development, empty};
+  const activeScenario = ENV.mirageScenario;
   const scenario = scenarios[activeScenario];
-
-  if (scenario) {
-    scenario(server);
-  }
-  else {
-    // category with items
-    server.createList('item-category', 2).map(category => {
-      server.createList('item', 3, {category});
-    });
-    // category without items
-    server.createList('item-category', 1);
-    // items without categories
-    server.createList('item', 3);
-  }
+  if (scenario) scenario(server);
 }
--- mirage/scenarios/empty-list.js
+++
@@ -1,4 +0,0 @@
-export default function(server) {
-  // category with items
-  server.createList('item', 0)
-}

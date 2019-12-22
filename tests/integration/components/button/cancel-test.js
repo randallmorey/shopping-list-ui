f0255059e8c7411fb0a42871fb547237cc7eab3f
Randall Morey <randall@randallmorey.com>
2019-12-22 11:32:14 -0500

    feat: 🎸 add cancel button

--- tests/integration/components/button/cancel-test.js
+++ tests/integration/components/button/cancel-test.js
@@ -4,40 +4,40 @@ import { render, find } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import setupI18nMockService from '../../../helpers/mock-i18n';
 
-module('Integration | Component | button/save', function(hooks) {
+module('Integration | Component | button/cancel', function(hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(() => {
     setupI18nMockService({
       actions: {
         default: {
-          save: "Mock Save"
+          cancel: "Mock Cancel"
         }
       }
     });
   });
 
-  test('it renders a submit button', async function(assert) {
+  test('it renders a non-submit button', async function(assert) {
     assert.expect(1);
-    await render(hbs`<Button::Save>Test</Button::Save>`);
-    assert.ok(find('button[type="submit"]'), 'Button is a submit');
+    await render(hbs`<Button::Cancel>Test</Button::Cancel>`);
+    assert.ok(find('button:not([type="submit"])'), 'Button is not a submit');
   });
 
   test('it renders default content when no block', async function(assert) {
     assert.expect(1);
-    await render(hbs`<Button::Save />`);
-    assert.equal(this.element.textContent.trim(), 'Mock Save');
+    await render(hbs`<Button::Cancel />`);
+    assert.equal(this.element.textContent.trim(), 'Mock Cancel');
   });
 
   test('it renders block content', async function(assert) {
     assert.expect(1);
-    await render(hbs`<Button::Save>Hello World</Button::Save>`);
+    await render(hbs`<Button::Cancel>Hello World</Button::Cancel>`);
     assert.equal(this.element.textContent.trim(), 'Hello World');
   });
 
   test('it supports a loading state', async function(assert) {
     assert.expect(1);
-    await render(hbs`<Button::Save @loading={{true}} />`);
+    await render(hbs`<Button::Cancel @loading={{true}} />`);
     assert.ok(find('.is-loading'), 'Button has is-loading class');
   });
 });

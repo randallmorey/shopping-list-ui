37f2d7c442d2ad173c670732a8ee97993fddf44e
Randall Morey <randall@randallmorey.com>
2019-12-25 10:18:09 -0500

    refactor: 💡 move auto date fields and standard mixins into base

--- app/models/item-category.js
+++ app/models/item-category.js
@@ -1,26 +1,14 @@
-import Model from '@ember-data/model';
+import Model from './model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
-import Validator from '../mixins/object-validator';
-import DatedModelAttributesMixin from '../mixins/dated-model-attributes';
-import DatedModelMethodsMixin from '../mixins/dated-model-attributes';
 
 /**
  * An item category.
  *
- * @class ItemModel
- * @augments Model
- * @augments Validator
- * @augments DatedModelAttributesMixin
- * @augments DatedModelMethodsMixin
- *
  * @property name {String}
  * @property items {ItemModel[]}
  */
-export default class ItemCategoryModel extends Model.extend(
-  Validator,
-  DatedModelAttributesMixin,
-  DatedModelMethodsMixin) {
+export default class ItemCategoryModel extends Model {
 
   // =attributes
 
---
+++ app/models/model.js
@@ -0,0 +1,40 @@
+import Model from '@ember-data/model';
+import attr from 'ember-data/attr';
+import Validator from '../mixins/object-validator';
+import TrackedModelRelationshipsMixin from
+  '../mixins/tracked-model-relationships';
+
+/**
+ * A convenience base model that extends common mixins (validator, tracked
+ * relationships), and provides auto-date fields `dateCreated`
+ * and `dateUpdated`.
+ */
+export default class ModelModel extends Model.extend(
+  Validator,
+  TrackedModelRelationshipsMixin) {
+
+  // =attributes
+
+  @attr('date') dateCreated;
+  @attr('date') dateUpdated;
+
+  // =methods
+
+  /**
+   * Updates `dateUpdated` on every save and adds `dateCreated` on first save.
+   * Deleted models are not updated.
+   */
+  setAutoDateFields() {
+    const now = new Date();
+    if (this.isNew && !this.isDeleted) this.setProperties({dateCreated: now});
+    if (!this.isDeleted) this.setProperties({dateUpdated: now});
+  }
+
+  /**
+   *
+   */
+  save() {
+    this.setAutoDateFields();
+    return super.save(...arguments);
+  }
+}

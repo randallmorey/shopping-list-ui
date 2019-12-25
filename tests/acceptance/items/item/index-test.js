37609d59864fbf9e94251eebe3fe8fdaf4f19d84
fernandogelin <fernando@fernandogelin.com>
2019-12-25 10:27:55 -0500

    test: 💍 add items tests

--- tests/acceptance/items/item/index-test.js
+++ tests/acceptance/items/item/index-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, find, click, fillIn, settled } from '@ember/test-helpers';
+import { visit, currentURL, find, findAll, click, fillIn, settled } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
@@ -43,31 +43,49 @@ module('Acceptance | items/item/index', function(hooks) {
     await visit(url);
     await fillIn('[name="name"]', '');
     await click('.button-save');
+    await a11yAudit();
     assert.equal(find('.help.is-danger').textContent.trim(), 'This field is required.', 'Error message is present.');
     assert.equal(currentURL(), url, 'Remained on current route')
   });
 
-  test('can see error message for invalid record (e.g. with blank name field)', async function(assert) {
-    // This is the same as the test above...
-    const item = this.server.create('item', 1);
-    const url = `/items/${item.id}`;
-    assert.expect(1);
-    await visit(url);
-    await fillIn('[name="name"]', '');
-    await click('.button-save');
-    assert.equal(find('.help.is-danger').textContent.trim(), 'This field is required.', 'Error message is present.');
-    await a11yAudit();
-  });
-
   test('changing record\'s category moves it within the items list', async function(assert) {
-    assert.expect(0);
-    // TODO
+    assert.expect(6);
+    let items = [];
+    this.server.createList('item-category', 2).map(category => {
+      items.push(this.server.create('item', 1, {category}));
+    });
+    const url = `/items/${items[0].id}`;
+    await visit(url);
+    assert.equal(currentURL(), url)
+    let lists = findAll('.pane:first-child .list-group ul.list').map(group => group.children.length)
+    assert.equal(lists.length, 2, 'Two list groups for categories exist.')
+    assert.equal(lists[0], 1, 'First list group has one item.')
+    assert.equal(lists[1], 1, 'Second list group has one item.')
+    await click(findAll('.pane:nth-child(2) .list-group ul.list label')[1])
+    lists = findAll('.pane:first-child .list-group ul.list').map(group => group.children.length)
+    assert.equal(lists.length, 1, 'Only one list group exist after changing one item category.')
+    assert.equal(lists[0], 2, 'List group has now 2 items')
+
     // do an a11y test once the category is changed
   });
 
   test('can change name and category and save record', async function(assert) {
-    assert.expect(0);
-    // TODO
+    assert.expect(5);
+    let items = [];
+    this.server.createList('item-category', 2).map(category => {
+      items.push(this.server.create('item', 1, {category}));
+    });
+    const url = `/items/${items[0].id}`;
+    await visit(url);
+    await fillIn('[name="name"]', 'test name');
+    assert.equal(items[0].category.name, findAll('.pane:nth-child(2) .list-group ul.list label')[0].textContent.trim())
+    await click(findAll('.pane:nth-child(2) .list-group ul.list label')[1])
+    await click('.button-save');
+    assert.equal(currentURL(), url);
+    assert.equal(findAll('.pane:first-child .list-group ul.list li')[1].textContent.trim(), 'test name')
+    const lists = findAll('.pane:first-child .list-group ul.list').map(group => group.children.length)
+    assert.equal(lists.length, 1, 'Only one list after saving.')
+    assert.equal(lists[0], 2, 'List has 2 items.')
   });
 
   test('redirects to /items after save', async function(assert) {

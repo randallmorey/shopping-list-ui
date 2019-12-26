c4d1c95fc171ef04f8969945c7e902d0648b298e
Randall Morey <randall@randallmorey.com>
2019-12-25 20:03:52 -0500

    refactor: 💡 move actions around for convenience
    We need to build a new item route, which has most persistence actions in
    common with the item route.  We also need a generic abandon confirmation
    action, as this function is needed across the app.

--- app/routes/items/item/index.js
+++ app/routes/items/item/index.js
@@ -1,15 +1,9 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 /**
  * Handles item persistence and post-persistence actions.
  */
 export default class ItemsItemIndexRoute extends Route {
-
-  // =services
-
-  @service confirmations;
 
   // =methods
 
@@ -22,71 +16,4 @@ export default class ItemsItemIndexRoute extends Route {
     controller.set('categories', this.modelFor('items').categories);
   }
 
-  // =actions
-
-  /**
-   * If dirty, valid, and not already saving, save the record.
-   * If save is successful, redirect to items.
-   */
-  @action
-  async submit() {
-    // Only do save if the model has changes that are valid.
-    const canSave =
-      this.currentModel.isDirty &&
-      this.currentModel.validate() &&
-      !this.currentModel.isSaving;
-
-    /* istanbul ignore else  */
-    if (canSave) {
-      await this.currentModel.save();
-      this.transitionTo('items');
-    }
-  }
-
-  /**
-   * Clear errors, rollback attributes and relationships,
-   * and redirect to items route.  Redirect to items.
-   */
-  @action
-  cancel() {
-    this.currentModel.errors.clear();
-    this.currentModel.rollback();
-    this.transitionTo('items');
-  }
-
-  /**
-   * Requests delete confirmation from user.  If confirmed, destroys
-   * record and redirects to items.
-   */
-  @action
-  delete() {
-    /* istanbul ignore else  */
-    if (!this.deleteConfirmation) {
-      this.deleteConfirmation = this.confirmations.getConfirmation('delete');
-      this.deleteConfirmation
-        .then(() => this.currentModel.destroyRecord())
-        .then(() => this.transitionTo('items'))
-        .finally(() => delete this.deleteConfirmation);
-    }
-  }
-
-  /**
-   * Requests abandon confirmation from user if record has changes.
-   * @param {Transition} transition
-   */
-  @action
-  willTransition(transition) {
-    /* istanbul ignore else  */
-    if (this.currentModel.isDirty && !this.abandonConfirmation) {
-      transition.abort();
-      this.abandonConfirmation = this.confirmations.getConfirmation('abandon');
-      this.abandonConfirmation
-        .then(() => {
-          this.currentModel.errors.clear();
-          this.currentModel.rollback();
-          transition.retry();
-        })
-        .finally(() => delete this.abandonConfirmation);
-    }
-  }
 }
--- tests/unit/routes/items/item/index-test.js
+++ tests/unit/routes/items/item/index-test.js
@@ -1,6 +1,5 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import { resolve } from 'rsvp';
 
 module('Unit | Route | items/item/index', function(hooks) {
   setupTest(hooks);
@@ -8,34 +7,5 @@ module('Unit | Route | items/item/index', function(hooks) {
   test('it exists', function(assert) {
     let route = this.owner.lookup('route:items/item/index');
     assert.ok(route);
-  });
-
-  test('it\'s submit action saves the record and redirects if dirty and valid and not saving', function(assert) {
-    const store = this.owner.lookup('service:store');
-    const model = store.createRecord('item', {name: 'Test'});
-    const route = this.owner.lookup('route:items/item/index');
-    assert.expect(5);
-    route.currentModel = model;
-    route.transitionTo = routeName => assert.equal(routeName, 'items');
-    model.save = () => {
-      assert.ok(true, 'model.save was called');
-      return resolve();
-    };
-    assert.ok(model.isDirty);
-    assert.ok(model.validate());
-    assert.notOk(model.isSaving);
-    route.submit();
-  });
-
-  test('it\'s cancel action cleans the record and redirects', function(assert) {
-    const store = this.owner.lookup('service:store');
-    const model = store.createRecord('item', {});
-    const route = this.owner.lookup('route:items/item/index');
-    assert.expect(3);
-    route.currentModel = model;
-    route.transitionTo = routeName => assert.equal(routeName, 'items');
-    assert.ok(model.isDirty);
-    route.cancel();
-    assert.ok(model.isClean);
   });
 });

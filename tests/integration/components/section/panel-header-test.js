5e1c65aaf92df69b50f14d0b70ddfed14d66f7dd
Randall Morey <randall@randallmorey.com>
2019-12-28 19:57:24 -0500

    refactor: 💡 section panel header should have 3 columns

--- tests/integration/components/section/panel-header-test.js
+++ tests/integration/components/section/panel-header-test.js
@@ -7,15 +7,24 @@ module('Integration | Component | section/panel-header', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    assert.expect(2);
+    assert.expect(7);
     await render(hbs`
       <Section::PanelHeader as |header|>
-        <header.title>
-          Title
-        </header.title>
+        <header.columnLeft>Left</header.columnLeft>
+        <header.columnCenter>
+          <header.title>
+            Title
+          </header.title>
+        </header.columnCenter>
+        <header.columnRight>Right</header.columnRight>
       </Section::PanelHeader>
     `);
-    assert.equal(this.element.textContent.trim(), 'Title');
+    assert.ok(find('.section-panel-header-column-left'));
+    assert.ok(find('.section-panel-header-column-center'));
+    assert.ok(find('.section-panel-header-column-right'));
     assert.ok(find('.section-panel-header'));
+    assert.equal(find('.section-panel-heading').textContent.trim(), 'Title');
+    assert.equal(find('.section-panel-header-column-left').textContent.trim(), 'Left');
+    assert.equal(find('.section-panel-header-column-right').textContent.trim(), 'Right');
   });
 });

6a109977b5adc0f9dea7213b71b608002068e413
Randall Morey <randall@randallmorey.com>
2019-12-29 17:58:01 -0500

    refactor: 💡 extract item form into component

---
+++ tests/integration/components/item/form-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | item/form', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    this.noop = () => {};
+
+    await render(hbs`<Item::Form
+      @title="Title"
+      @submit={{this.noop}}
+      @cancel={{this.noop}}
+      @delete={{this.noop}}
+      />`);
+
+    assert.ok(find('form'));
+  });
+});

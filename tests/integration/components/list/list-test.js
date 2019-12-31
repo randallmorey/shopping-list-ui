2a58f7a31c95f1deee37c90de63a775876cd21a4
Randall Morey <randall@randallmorey.com>
2019-12-31 13:37:25 -0500

    feat: 🎸 lists may be ordered

--- tests/integration/components/list/list-test.js
+++ tests/integration/components/list/list-test.js
@@ -1,26 +1,31 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | list/list', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<List::List />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('it renders unordered list by default', async function(assert) {
+    assert.expect(2);
     await render(hbs`
-      <List::List>
-        template block text
+      <List::List as |list|>
+        <list.item>template block text</list.item>
       </List::List>
     `);
-
+    assert.ok(find('ul'));
     assert.equal(this.element.textContent.trim(), 'template block text');
   });
+
+  test('it can render an ordered list', async function(assert) {
+    assert.expect(2);
+    await render(hbs`
+      <List::List @ordered={{true}} as |list|>
+        <list.item>template block text</list.item>
+      </List::List>
+    `);
+    assert.ok(find('ol'));
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+
 });

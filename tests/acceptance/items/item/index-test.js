9cb694019eee8f6bc076f592f99391f599ff308c
Randall Morey <randall@randallmorey.com>
2020-01-01 14:20:45 -0500

    test: add todos

--- tests/acceptance/items/item/index-test.js
+++ tests/acceptance/items/item/index-test.js
@@ -75,28 +75,37 @@ module('Acceptance | items/item/index', function(hooks) {
     assert.equal(lists.length, 1, 'Only one list group exist after changing one item category.')
     assert.equal(lists[0], 2, 'List group has now 2 items')
 
-    // do an a11y test once the category is changed
+    // TODO do an a11y test once the category is changed
   });
 
+  // TODO probably need to manually generate names for category in this case
   test('can change name and category and save record', async function(assert) {
     assert.expect(5);
+    // TODO Don't call this variable "items" in a test about items
     let items = [];
     const itemCategories = this.server.createList('item-category', 2)
     let sortedItemCategories = itemCategories.sort((a, b) => {
-      return b.name < a.name;
+      return a.name < b.name;
     });
     sortedItemCategories.map(category => {
+      // TODO push should be avoided and can usually be rewritten in terms of map or reduce
       items.push(this.server.create('item', 1, {category}));
     });
     const url = `/items/${items[0].id}`;
     await visit(url);
     await fillIn('[name="name"]', 'Test Category Name');
+    // TODO simplify selector and use "find" to get only the first match
     assert.equal(items[0].category.name, findAll('.pane:nth-child(2) .list-group ul.list label')[0].textContent.trim())
+    // TODO simplify selector
+    // TODO use a selector if possible, instead of passing an element to click
     await click(findAll('.pane:nth-child(2) .list-group ul.list label')[1])
     await click('.button-save');
     await settled();
     assert.equal(currentURL(), '/items');
+    // TODO simplify selector
     assert.equal(findAll('.pane:first-child .list-group ul.list li')[1].textContent.trim(), 'Test Category Name')
+    // TODO simplify selector
+    // TODO variable name does not reflect what it is (it isn't lists, it's list lengths)
     const lists = findAll('.pane:first-child .list-group ul.list').map(group => group.children.length)
     assert.equal(lists.length, 1, 'Only one list after saving.')
     assert.equal(lists[0], 2, 'List has 2 items.')

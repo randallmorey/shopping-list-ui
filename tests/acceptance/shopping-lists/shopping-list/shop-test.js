7d6ff0f766c9b18b63e8caf5d31dae98665ef979
fernandogelin <fernando@fernandogelin.com>
2020-01-11 13:13:59 -0500

    test: 💍 add toggle action in shop acceptance test
    Closes: #65

--- tests/acceptance/shopping-lists/shopping-list/shop-test.js
+++ tests/acceptance/shopping-lists/shopping-list/shop-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, click, currentURL, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
@@ -21,6 +21,22 @@ module('Acceptance | shopping lists/shopping list/shop', function(hooks) {
     await visit('/lists/1/shop');
     await a11yAudit();
     assert.equal(currentURL(), '/lists/1/shop');
+  });
+
+  test('can toogle purchased item using checkbox', async function(assert) {
+    assert.expect(4);
+    const storeProperty = this.server.create('store');
+    const itemCategory = this.server.create('item-category');
+    this.server.create('store-item-category', {storeProperty, itemCategory});
+    const list = this.server.create('shopping-list');
+    const item = this.server.create('item', {category: itemCategory});
+    this.server.create('shopping-list-item', {list, item, quantity: 1, purchased: false});
+    await visit('/lists/1/shop');
+    assert.notOk(find('.tag-inverted'));
+    assert.equal(this.server.db.shoppingListItems[0].purchased, false);
+    await click('.list-item-link');
+    assert.ok(find('.tag-inverted'));
+    assert.equal(this.server.db.shoppingListItems[0].purchased, true);
   });
 
 });
